10522b73e97921a5617badb7e881ce7803aa33b1
Seif Younes <seif99y@gmail.com>
2020-07-18 09:20:53 +1200

    Fixed formatting of items in nav bar

--- schmeet-app/src/App.js
+++ schmeet-app/src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import "./App.css";
 import DateFnsUtils from "@date-io/date-fns";
-import { AppBar, Avatar, Button, Toolbar } from "@material-ui/core";
-import logoLight from "./images/schmeet-light.png";
-import logoDark from "./images/schmeet-dark.png";
 import NavBar from "./components/NavBar.jsx";
 
 function App() {
--- schmeet-app/src/components/NavBar.jsx
+++ schmeet-app/src/components/NavBar.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "../App.css";
-import { AppBar, Avatar, Button, Toolbar } from "@material-ui/core";
+import { AppBar, Avatar, Button, Grid, Toolbar } from "@material-ui/core";
 import logoLight from "../images/schmeet-light.png";
 import logoDark from "../images/schmeet-dark.png";
 
-function App() {
+function NavBar() {
 	return (
 		<div className="NavBar">
 			<AppBar className="TopNavbar" color="White">
 				<Toolbar>
-					<React.Fragment>
-						<img className="NavBarItem" src={logoLight}/>
-					</React.Fragment>
-					<React.Fragment style={{alignContent: "right"}}>
-						<Button className="NavBarItem" variant="contained" color="primary" style={{textAlign: "right"}}>
-							New Meeting
-						</Button>
-						<Avatar className="NavBarItem" style={{textAlign: "right"}}/>
-					</React.Fragment>
+                    <Grid container justify="space-between" >
+                        <Grid item xs={6}>
+                            <img className="NavBarItem" src={logoLight} style={{float: "left"}}/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Avatar className="NavBarItem" style={{float: "right", marginLeft: "30px"}}/>
+                            <Button className="NavBarItem" variant="contained" color="primary" style={{float: "right"}}>
+                                New Meeting
+                            </Button>
+                        </Grid>
+                    </Grid>
 				</Toolbar>
 			</AppBar>
 		</div>
@@ -25,4 +27,4 @@ function App() {
 
 }
 
-export default App;
+export default NavBar;
