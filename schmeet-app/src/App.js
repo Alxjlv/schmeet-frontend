03f2d1fe37710bce127b419f147ba677a6ef400c
Seif Younes <seif99y@gmail.com>
2020-07-17 21:07:04 +1200

    Added logos and started working on navbar

--- schmeet-app/src/App.js
+++ schmeet-app/src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import "./App.css";
 import DateFnsUtils from "@date-io/date-fns";
+import AppBar from "@material-ui/core/AppBar";
+import logoLight from "./images/schmeet-light.png";
+import logoDark from "./images/schmeet-dark.png"
 
 function App() {
 	return (
 		<div className="App">
+			<AppBar className="TopNavbar" position="static" color="White">
+				<img src={logoLight} width="200px"/>
+			</AppBar>
 			<MuiPickersUtilsProvider utils={DateFnsUtils}>
 				<DateTimePicker />
 			</MuiPickersUtilsProvider>
 		</div>
 	);
+
 }
 
 export default App;
