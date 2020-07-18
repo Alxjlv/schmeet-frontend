84ed5a49f2e63fe420667b9279217d36ec8fd2f2
Seif Younes <seif99y@gmail.com>
2020-07-18 20:45:15 +1200

    Changed input for meeting length to a drop-down list

--- schmeet-app/src/components/NewMeeting.jsx
+++ schmeet-app/src/components/NewMeeting.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../App.css";
-import { Grid, CssBaseline, TextField, Typography, Button } from "@material-ui/core";
+import { Grid, CssBaseline, FormControl, FormHelperText, InputLabel, NativeSelect, TextField, Typography, Button } from "@material-ui/core";
 import BaseCalendar from "./BaseCalendar";
 import InviteField from "./InviteField";
 // import { ReactComponent } from "*.svg";
@@ -64,11 +64,22 @@ class NewMeeting extends React.Component {
 						/>
 					</Grid>
 					<Grid item xs={12}>
-						<TextField
-							id="length"
-							label="Length (in minutes)"
-							className="TextInput"
-						/>
+					<FormControl className="TextInput">
+						<InputLabel htmlFor="age-native-helper">Age</InputLabel>
+						<NativeSelect
+						inputProps={{
+							name: 'age',
+							id: 'age-native-helper',
+						}}
+						>
+						<option aria-label="None" value="" />
+						<option value={30}>30</option>
+						<option value={60}>60</option>
+						<option value={90}>90</option>
+						<option value={120}>120</option>
+						</NativeSelect>
+						<FormHelperText>Length (in minutes)</FormHelperText>
+					</FormControl>
 					</Grid>
 					<Grid item xs={12}>
 						{isShowCalendar
