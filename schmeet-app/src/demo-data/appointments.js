c6c0a6277da3dbe0aed24dd1af4472e1e4419e34
Aiden Burgess <aiden9874@gmail.com>
2020-07-18 14:37:23 +1200

    Add some break times

--- schmeet-app/src/demo-data/appointments.js
+++ schmeet-app/src/demo-data/appointments.js
@@ -22,31 +22,51 @@ export const appointments = [
 	},
 	{
 		title: "Break Time",
+		startDate: new Date(2020, 7, 17, 12, 0),
+		endDate: new Date(2020, 7, 17, 13, 0),
+		id: 0,
+		location: "Room 1",
+		description: "Take a break mate",
+		link:
+			"https://zoom.us/j/91218086919?pwd=SGpjMFhGMGhIMTV4QzYxVy9aelZzZz09",
+	},
+	{
+		title: "Break Time",
+		startDate: new Date(2020, 7, 18, 12, 0),
+		endDate: new Date(2020, 7, 18, 13, 0),
+		id: 0,
+		location: "Room 1",
+		description: "Take a break mate",
+		link:
+			"https://zoom.us/j/91218086919?pwd=SGpjMFhGMGhIMTV4QzYxVy9aelZzZz09",
+	},
+	{
+		title: "Break Time",
+		startDate: new Date(2020, 7, 19, 12, 0),
+		endDate: new Date(2020, 7, 19, 13, 0),
+		id: 0,
+		location: "Room 1",
+		description: "Take a break mate",
+		link:
+			"https://zoom.us/j/91218086919?pwd=SGpjMFhGMGhIMTV4QzYxVy9aelZzZz09",
+	},
+	{
+		title: "Break Time",
 		startDate: new Date(2020, 7, 20, 12, 0),
 		endDate: new Date(2020, 7, 20, 13, 0),
 		id: 0,
 		location: "Room 1",
-		description: "Go and watch the hamilton movie woooooo!!",
+		description: "Take a break mate",
 		link:
 			"https://zoom.us/j/91218086919?pwd=SGpjMFhGMGhIMTV4QzYxVy9aelZzZz09",
 	},
 	{
-		title: "Watch Hamilton",
-		startDate: new Date(2020, 7, 20, 17, 35),
-		endDate: new Date(2020, 7, 20, 20, 30),
+		title: "Break Time",
+		startDate: new Date(2020, 7, 21, 12, 0),
+		endDate: new Date(2020, 7, 21, 13, 0),
 		id: 0,
 		location: "Room 1",
-		description: "Go and watch the hamilton movie woooooo!!",
-		link:
-			"https://zoom.us/j/91218086919?pwd=SGpjMFhGMGhIMTV4QzYxVy9aelZzZz09",
-	},
-	{
-		title: "Watch Hamilton",
-		startDate: new Date(2020, 7, 20, 17, 35),
-		endDate: new Date(2020, 7, 20, 20, 30),
-		id: 0,
-		location: "Room 1",
-		description: "Go and watch the hamilton movie woooooo!!",
+		description: "Take a break mate",
 		link:
 			"https://zoom.us/j/91218086919?pwd=SGpjMFhGMGhIMTV4QzYxVy9aelZzZz09",
 	},
